4b4486512f4ebe68f027674ef3533693ec8083df
Mintlord3ttah <lordmint7@gmail.com>
2025-05-13 08:10:58 -0700

    responsive design, add new features, fix bugs

--- BACKEND/utils/apiHandlers.js
+++ BACKEND/utils/apiHandlers.js
@@ -6,7 +6,9 @@ class API{
     }
 
     create(){
-        this.query = this.query.create(this.req.body)
+        
+        this.query =this.req.body.length > 1 ? this.query.insertMany(this.req.body) :
+                    this.query.create(this.req.body)
         // try{
         //         const user = await this.query.create(this.req.body)
         //         this.res.status(201).json({
