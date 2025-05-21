01ed7210ed718668ce44e582a087d63d6aa92aad
Mintlord3ttah <lordmint7@gmail.com>
2025-05-21 05:35:01 -0700

    Error: secretOrPrivateKey must have a value - fix

--- BACKEND/utils/generateUserTokens.js
+++ BACKEND/utils/generateUserTokens.js
@@ -3,11 +3,10 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: "./config.env" });
 
-
 export async function generateUserTokens(user, res){
     const expTime = Math.floor(Date.now() / 1000); // 15 minutes
-    const accessToken = jwt.sign({id: user._id}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: expTime + 900}) // 15 minutes
-    const refreshToken = jwt.sign({id: user._id}, process.env.REFRESH_TOKEN_SECRET, {expiresIn: expTime + 604800}) // 7 days
+    const accessToken = jwt.sign({id: user._id}, process.env.ACCESS_TOKEN_SECRET || "accesstokensecret1256790", {expiresIn: expTime + 900}) // 15 minutes
+    const refreshToken = jwt.sign({id: user._id}, process.env.REFRESH_TOKEN_SECRET || "refreshtokensecret1256790", {expiresIn: expTime + 604800}) // 7 days
 
     if(!refreshToken) throw new Error("Failed to generate refresh token")
     await res.cookie("refreshToken", refreshToken, {
