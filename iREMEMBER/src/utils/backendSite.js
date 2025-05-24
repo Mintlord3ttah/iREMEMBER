9ac091211ede4ef64ccf24f61040e736c54b880d
Mintlord3ttah <lordmint7@gmail.com>
2025-05-24 04:41:07 -0700

    BACKEND_URL - import error fixed

--- iREMEMBER/src/utils/backendSite.js
+++ iREMEMBER/src/utils/backendSite.js
@@ -1,2 +1,2 @@
 // export const BACKEND_URL = "http://localhost:3000/api/v1";
-export const VITE_BACKEND_URL= "https://irem-backend.onrender.com/api/v1"
+export const BACKEND_URL= "https://irem-backend.onrender.com/api/v1"
